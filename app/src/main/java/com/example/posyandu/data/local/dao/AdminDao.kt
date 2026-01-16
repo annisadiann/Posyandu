4b0767a54d0ebe8142353840a485ce7d0d7aa3f2
annisadiann <annisadian0412@gmail.com>
2026-01-16 21:10:05 +0700

    add file AdminDao.kt

---
+++ app/src/main/java/com/example/posyandu/data/local/dao/AdminDao.kt
@@ -0,0 +1,19 @@
+package com.example.posyandu.data.local.dao
+
+import androidx.room.*
+import com.example.posyandu.data.local.entity.Admin
+
+@Dao
+interface AdminDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun register(admin: Admin) // REQ-REG-3 [cite: 174]
+
+    @Query("SELECT * FROM admin WHERE username = :username LIMIT 1")
+    suspend fun login(username: String): Admin? // REQ-LOG-2 [cite: 190]
+
+    @Query("SELECT * FROM admin WHERE id_admin = :id LIMIT 1")
+    suspend fun getAdminById(id: Int): Admin?
+
+    @Query("SELECT COUNT(*) FROM admin")
+    suspend fun getAdminCount(): Int
+}
